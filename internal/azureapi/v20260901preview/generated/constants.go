1fcef3f2bc174592d57668099481bcab139a3ca0
Matthew Barnes <mbarnes@home.arpa>
2026-08-06 14:07:44 -0400

    refactor(api): Drop workaround for patch defaults
    Since we're not using the ArmResourcePatchAsync template anymore,
    https://github.com/Azure/typespec-azure/issues/1586 is no longer
    relevant for us. We can drop the workarounds.

--- internal/azureapi/v20260901preview/generated/constants.go
+++ internal/azureapi/v20260901preview/generated/constants.go
@@ -451,7 +451,17 @@ func PossibleTokenValidationRuleTypeValues() []TokenValidationRuleType {
 	}
 }
 
-// UsernameClaimPrefixPolicy - UsernameClaimPrefixPolicy configures whether to add a prefix to a JWT claim.
+// UsernameClaimPrefixPolicy - Prefix policy is an optional field that configures how a prefix should be applied to the value
+// of the JWT claim specified in the 'claim' field.
+// Allowed values are 'Prefix', 'NoPrefix', and 'None'. If not specified, the default policy is 'None'.
+// When set to 'Prefix', the value specified in the prefix field will be prepended to the value of the JWT claim. The prefix
+// field must be set when prefixPolicy is 'Prefix'.
+// When set to 'NoPrefix', no prefix will be prepended to the value of the JWT claim.
+// When set to 'None', this means no opinion and the platform is left to choose any prefixes that are applied which is subject
+// to change over time. Currently, the platform prepends {issuerURL}# to the
+// value of the JWT claim when the claim is not 'email'. As an example, consider the following scenario:prefix is unset, issuerURL
+// is set to https://myoidc.tld, the JWT claims include "username":"userA"
+// and "email":"userA
 type UsernameClaimPrefixPolicy string
 
 const (
